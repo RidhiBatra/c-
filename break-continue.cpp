6e1c8232b970a024eb9e40396656d10dc781ee99
Ridhi Batra <119876916+RidhiBatra@users.noreply.github.com>
2024-03-25 20:40:13 +0530

    Add files via upload

---
+++ break-continue.cpp
@@ -0,0 +1,48 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int pocketmoney=3000;
+    for(
+        int date=1;date<=30;date++ ){
+        if(date%2==0){
+            continue;}
+        if(pocketmoney==0){break;
+    }
+    cout<<"go out today!"<<endl;
+    pocketmoney=pocketmoney-300
+}
+return 0;
+}*/
+
+//print all those numbers not divisible by 3 from 1-100
+/*#include<iostream>
+using namespace std;
+int main(){
+    for(
+        int i=0; i<100; i++
+    ){
+        if(i%3==0){
+            continue;
+        }
+        cout<<i<<endl;
+    }return 0;
+}*/
+
+
+//check the given no. prime or not
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i;
+    for(i=2;i<n;i++){
+        if(n%i==0){
+            cout<<"non prime"<<endl;
+        }
+    }
+    if{i==n}{
+    cout<<"prime"<<endl;}
+    return 0;
+}*/
