11aae1467d1bfadc8de0d2d6a5dc4c4dae028876
Ridhi Batra <119876916+RidhiBatra@users.noreply.github.com>
2024-04-16 18:12:11 +0530

    Add files via upload

---
+++ if-else.cpp
@@ -0,0 +1,58 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    int savings;
+    cin>>savings;
+    if (savings>5000){
+        cout<<"Neha\n";
+
+    }else{
+        cout<<"Rashmi\n";
+    }
+    return 0;
+}*/
+
+
+
+
+
+//how to find a maximum from 3 given numbers.
+/*#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b){
+        if (a>c){
+            cout<<a<<endl;
+        }
+        else{
+            cout<<c<<endl;
+        }
+    }
+    else{
+        if(b>c){
+            cout<<b<<endl;
+        }
+        else{
+            cout<<c<<endl;
+        }
+    }
+}*/
+
+
+//how to find even and odd of the given numbers//
+#include<iostream>
+using namespace std;
+int main(
+    {
+        int n;
+        cin>>n;
+        if(n%2==0){
+            cout<<"Even"<<endl;
+        }
+        else{
+            cout<<"odd"<<endl;
+        }
+    }
+)
