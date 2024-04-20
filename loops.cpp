e6c1898db6acfbe59bcb2b9219576f315918c0de
Ridhi Batra <119876916+RidhiBatra@users.noreply.github.com>
2024-04-20 08:47:38 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,46 @@
+//for loops
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    int sum=0;
+    for(int counter=1;counter<=n;counter++){
+        sum=sum+counter;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
+*/
+
+
+
+//while loops terminate a loop until user give a negative number
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    int sum=0;
+    for(int counter=1;counter<=n;counter++){
+        sum=sum+counter;
+    }
+    cout<<sum<<endl;
+    return 0;
+}*/
+
+
+
+//do while loop
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    int sum=0;
+    for(int counter=1;counter<=n;counter++){
+        sum=sum+counter;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
