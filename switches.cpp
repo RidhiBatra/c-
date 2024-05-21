9f2a59791a3d4d7b96556f8591de05a69bd68803
Ridhi Batra <119876916+RidhiBatra@users.noreply.github.com>
2024-05-21 09:40:05 +0530

    Add files via upload

---
+++ switches.cpp
@@ -0,0 +1,61 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    switch(button)
+    {
+    case 'a':
+        cout<<"Hello"<<endl;
+        break;
+        case 'b':
+        cout<<"Namaste"<<endl;
+        break;
+        case 'c':
+        cout<<"Salute"<<endl;
+        break;
+    default:
+    cout<<"I am still learning"<<endl;
+    break;
+
+    }
+   
+    
+    return 0;
+}*/
+
+
+
+
+//calculator
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cout<<"Input 2 numbers: ";
+    cin>>n1>>n2;
+
+    char op;
+    cout<<"input an operator: ";
+    cin>>op;
+    switch(op){
+        case '+':
+        cout<<n1+n2<<endl;
+        break;
+         case '-':
+        cout<<n1-n2<<endl;
+        break;
+         case '*':
+        cout<<n1*n2<<endl;
+        break;
+         case '/':
+        cout<<n1/n2<<endl;
+        break;
+
+        default:
+        cout<<"Calculate any other operator"<<endl;
+        break;
+
+    }
+    
+    return 0;
+    }*/
